eda9e981300f1917410cf49207da0364758a1517
pnttam90-cyber <pnttam90@gmail.com>
2026-02-15 11:55:01 +0700

    added model code

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,10 @@
 
-print('adding all the data processing code related fraud detection')
+print("adding all the data processing code related fraud detection")
+
+a = 5
+b = 7
+sum(a,b):
+    c = a+b
+    return c
+
+print("added a new function sum")
